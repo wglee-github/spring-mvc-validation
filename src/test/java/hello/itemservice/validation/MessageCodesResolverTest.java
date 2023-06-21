a6615b5d8327b949c904b6394e4c029294c2d9b4
wglee <amd0223@gmail.com>
2023-06-21 23:53:44 +0900

    섹션 4. 검증1 - Validation 9.오류 코드와 메시지 처리2 10.오류 코드와 메시지 처리3 11.오류 코드와 메시지 처리4 12.오류 코드와 메시지 처리5 13.오류 코드와 메시지 처리6 14.Validator 분리1 15.Validator 분리2

---
+++ src/test/java/hello/itemservice/validation/MessageCodesResolverTest.java
@@ -0,0 +1,34 @@
+package hello.itemservice.validation;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.validation.DefaultMessageCodesResolver;
+import org.springframework.validation.MessageCodesResolver;
+
+public class MessageCodesResolverTest {
+
+	MessageCodesResolver codesResolver = new DefaultMessageCodesResolver();
+	
+	@Test
+	void messageCodesResolverObject() {
+		String[] messageCodes = codesResolver.resolveMessageCodes("required", "item");
+		for (String code : messageCodes) {
+			System.out.println("code = " + code);
+		}
+		assertThat(messageCodes).containsExactly("required.item", "required");
+	}
+	
+	@Test
+	void messageCodesResolverField() {
+	 	String[] messageCodes = codesResolver.resolveMessageCodes("required", "item", "itemName", String.class);
+	 	for (String code : messageCodes) {
+	 		System.out.println("code = " + code);
+		}
+	 	assertThat(messageCodes).containsExactly(
+		 	"required.item.itemName",
+		 	"required.itemName",
+		 	"required.java.lang.String",
+		 	"required");
+	}
+}
